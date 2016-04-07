37d3137116bf7be40ddf4808c97c47261aaa085b
Anton Korenev <anton.korenev@sigma.software>
2016-04-07 15:07:31 +0300

    Solution to dead letters problem - GetData request was send to service by scheduler and in this case the response from service was send to nonactive actor.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -21,13 +21,11 @@ object Main {
 
     val client1 = system.scheduler.schedule(70 milliseconds, 70 milliseconds, aerospikeActor, MakePurchase(1, 10))
     val client2 = system.scheduler.schedule(50 milliseconds, 50 milliseconds, aerospikeActor, MakePurchase(2, 2))
-    val client2rec = system.scheduler.schedule(500 milliseconds, 500 milliseconds, aerospikeActor, GetData(2))
 
     TimeUnit.SECONDS.sleep(10)
 
     client1.cancel()
     client2.cancel()
-    client2rec.cancel()
 
     TimeUnit.SECONDS.sleep(10)
     aerospikeActor ! Stop
